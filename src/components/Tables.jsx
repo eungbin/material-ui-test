0e4ec07b33d8fc0fa92b77233f978dc7b8f41054
eungbin <programmer97@naver.com>
2022-06-23 17:06:54 +0900

    Delete checkbox at Tables

--- src/components/Tables.jsx
+++ src/components/Tables.jsx
@@ -9,7 +9,7 @@ const columns = [
     field: 'age',
     headerName: 'Age',
     type: 'number',
-    width: 90,
+    width: 130,
   },
   {
     field: 'fullName',
@@ -32,17 +32,26 @@ const rows = [
   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 10, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 11, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 12, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 13, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 14, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 15, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 16, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 17, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 18, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
 
 export default function DataTable() {
   return (
-    <div style={{ height: 400, width: '100%' }}>
+    <div style={{ height: 631, width: '100%' }}>
       <DataGrid
         rows={rows}
         columns={columns}
-        pageSize={5}
+        pageSize={10}
         rowsPerPageOptions={[5]}
-        checkboxSelection
+        bulkActionButtons={false}
       />
     </div>
   );
